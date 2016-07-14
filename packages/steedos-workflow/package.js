50815bb4257f2abdc3cbad41cdc173fa0bbb3b78
zhangshanshan2016 <zhangshanzhang@hotoa.com>
2016-07-14 18:34:31 +0800

    #289打包workflow到package speedos-workflow
    基本测试成功，功能细节还需要进一步测试

--- packages/steedos-workflow/package.js
+++ packages/steedos-workflow/package.js
@@ -19,30 +19,36 @@ Package.onUse(function(api) {
 	api.use('underscore');
 	api.use('tracker');
 	api.use('session');
-	
-	api.use('cfs:standard-packages');
-	api.use('nimble:restivus');
-	api.use('aldeed:simple-schema');
-	api.use('aldeed:collection2');
-	api.use('aldeed:tabular');
-	api.use('aldeed:autoform');
-	api.use('matb33:collection-hooks');
+    api.use('blaze');
+    api.use('templating');
+    api.use('steedos:lib');
+    api.use('flemay:less-autoprefixer@1.2.0');
+    api.use('simple:json-routes@2.1.0');
+    api.use('nimble:restivus@0.8.7');
+    api.use('aldeed:simple-schema@1.3.3');
+    api.use('aldeed:collection2@2.5.0');
+    api.use('aldeed:tabular@1.6.0');
+    api.use('aldeed:autoform@5.8.0');
+    api.use('matb33:collection-hooks@0.8.1');
+    api.use('cfs:standard-packages@0.5.9');
+    api.use('kadira:blaze-layout@2.3.0');
+    api.use('kadira:flow-router@2.10.1');
 
-	api.use('steedos:lib');
+    api.use('meteorhacks:ssr@2.2.0');
+    api.use('tap:i18n@1.7.0');
+    api.use('meteorhacks:subs-manager');
 
 	api.use(['webapp'], 'server');
 
 	api.use('momentjs:moment', 'client');
 	api.use('mrt:moment-timezone', 'client');
 
-	// TAPi18n
-	api.use('templating', 'client');
-
 	api.use('tap:i18n', ['client', 'server']);
 	//api.add_files("package-tap.i18n", ["client", "server"]);
 	tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
 	api.addFiles(tapi18nFiles, ['client', 'server']);
-	
+
+
 	// COMMON
 	api.addFiles('lib/collection_helpers.js');
 
@@ -59,6 +65,105 @@ Package.onUse(function(api) {
 	api.addFiles('lib/models/spaces.coffee');
 
 	api.addFiles('lib/cfs/core.coffee');
+
+    api.addFiles('client/lib/1_form_formula.js', 'client');
+    api.addFiles('client/lib/2_steedos_data_format.js', 'client');
+    api.addFiles('client/lib/approve_manager.js', 'client');
+    api.addFiles('client/lib/instance_manager.js', 'client');
+    api.addFiles('client/lib/steedos_util.js', 'client');
+    api.addFiles('client/lib/uuflow_api.js', 'client');
+    api.addFiles('client/lib/workflow_manager.js', 'client');
+
+    //add client file
+	api.addFiles('client/coreform/inputTypes/coreform-checkbox/boolean-checkbox.js', 'client');
+	api.addFiles('client/coreform/inputTypes/coreform-datepicker/coreform-datepicker.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-multiSelect/select-checkbox-inline.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-number/coreform-number.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-org/select-orgs.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-org/select-orgs.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-radio/select-radio-inline.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-section/steedos-section.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-section/steedos-section.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-table/steedos-table-modal.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-table/steedos-table-modal.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-table/steedos-table.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-table/steedos-table.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-table/steedos-table.less', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-textarea/coreform-textarea.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-textarea/coreform-textarea.js', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-user/select-users.html', 'client');
+    api.addFiles('client/coreform/inputTypes/coreform-user/select-users.js', 'client');
+
+    api.addFiles('client/views/instance/attachments.html', 'client');
+    api.addFiles('client/views/instance/attachments.js', 'client');
+    api.addFiles('client/views/instance/force_end_modal.html', 'client');
+    api.addFiles('client/views/instance/force_end_modal.js', 'client');
+    api.addFiles('client/views/instance/instance_button.html', 'client');
+    api.addFiles('client/views/instance/instance_button.coffee', 'client');
+    api.addFiles('client/views/instance/instance_form.html', 'client');
+    api.addFiles('client/views/instance/instance_form.coffee', 'client');
+    api.addFiles('client/views/instance/instance_form.less', 'client');
+
+    api.addFiles('client/views/instance/instance_suggestion.html', 'client');
+    api.addFiles('client/views/instance/instance_suggestion.coffee', 'client');
+    api.addFiles('client/views/instance/instance_view.html', 'client');
+    api.addFiles('client/views/instance/instance_view.coffee', 'client');
+    
+    api.addFiles('client/views/instance/reassign_modal.html', 'client');
+    api.addFiles('client/views/instance/reassign_modal.js', 'client');
+    api.addFiles('client/views/instance/relocate_modal.html', 'client');
+    api.addFiles('client/views/instance/relocate_modal.js', 'client');
+    api.addFiles('client/views/instance/traces.html', 'client');
+    api.addFiles('client/views/instance/traces.js', 'client');
+
+    api.addFiles('client/views/list/flow_list_box.html', 'client');
+    api.addFiles('client/views/list/flow_list_box.coffee', 'client');
+    api.addFiles('client/views/list/flow_list_box.less', 'client');
+
+    api.addFiles('client/views/list/flow_list_box_modal.html', 'client');
+    api.addFiles('client/views/list/flow_list_box_modal.coffee', 'client');
+
+
+    api.addFiles('client/views/list/flow_list_modal.html', 'client');
+    api.addFiles('client/views/list/flow_list_modal.coffee', 'client');
+    api.addFiles('client/views/list/flow_list_modal.less', 'client');
+
+    api.addFiles('client/views/list/instance_list.html', 'client');
+    api.addFiles('client/views/list/instance_list.coffee', 'client');
+
+    api.addFiles('client/views/list/monitor.html', 'client');
+    api.addFiles('client/views/list/monitor.js', 'client');
+
+    api.addFiles('client/views/home.html', 'client');
+    api.addFiles('client/views/home.coffee', 'client');
+
+
+    api.addFiles('client/views/menu.html', 'client');
+    api.addFiles('client/views/menu.coffee', 'client');
+    api.addFiles('client/views/menu.less', 'client');
+
+    api.addFiles('client/views/workflow_main.html', 'client');
+    api.addFiles('client/views/workflow_main.coffee', 'client');
+    api.addFiles('client/views/workflow_main.less', 'client');
+
+    api.addFiles('client/router.coffee', 'client');
+    api.addFiles('client/subscribe.coffee', 'client');
+
+    //add server file
+    api.addFiles('server/methods/get_instance_data.js', 'server');
+    api.addFiles('server/methods/save_instance.js', 'server');
+
+    api.addFiles('server/publications/categories.coffee', 'server');
+    api.addFiles('server/publications/cfs_instances.coffee', 'server');
+    api.addFiles('server/publications/flow_positions.coffee', 'server');
+    api.addFiles('server/publications/flow_roles.coffee', 'server');
+    api.addFiles('server/publications/flows.coffee', 'server');
+    api.addFiles('server/publications/forms.coffee', 'server');
+    api.addFiles('server/publications/instance_data.coffee', 'server');
+    api.addFiles('server/publications/instance_list.coffee', 'server');
+    api.addFiles('server/publications/my_spaces.coffee', 'server');
+
+    api.addFiles('tabular.coffee');
 
 	// EXPORT
 	api.export('Workflow');
