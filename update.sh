8f326df4e4ccc8c84a1b6049f0ad5f3a1e0786ca
Jack Zhuang <zhuangjianguo@me.com>
2016-09-02 11:36:01 +0800

    add submodules

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
 git pull
 git submodule update --init --recursive
+
 cd packages/steedos-cms
 git checkout master
 git pull
 
-cd ../..
+cd ../../
+cd packages/steedos-emailjs
+git checkout master
+git pull
+
+cd ../../
+cd packages/steedos-portal
+git checkout master
+git pull
+
+cd ../../
