75afc05ba9ade7e8b2b45907dfe787b5bcf297fe
sunhaolin <sunhaolin@hotoa.com>
2016-10-08 14:56:31 +0800

    update build file

--- build.bat
+++ build.bat
@@ -5,9 +5,6 @@ meteor build --server https://cn.steedos.com/workflow --directory ../apps-build/
 cd ../apps-build/bundle/programs/server
 rm -rf node_modules
 npm install --registry=https://registry.npm.taobao.org
-cd npm/npm-bcrypt/
-rm -rf node_modules
-npm install bcrypt --registry=https://registry.npm.taobao.org
 
 cd ../../
 node main.js
