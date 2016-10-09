ce72d9d72a507ccf7e6c1d6f300928c76fbde804
steedos <zhuangjianguo@hotlong.com>
2016-10-09 09:33:07 +0800

    theme background

--- packages/steedos-theme-qhd/package.js
+++ packages/steedos-theme-qhd/package.js
@@ -1,18 +1,23 @@
 Package.describe({
-    name: 'steedos:gongwen',
+    name: 'steedos:theme-qhd',
     version: '0.0.1',
-    summary: 'Steedos workflow libraries',
+    summary: 'Steedos theme for qhd',
     git: ''
 });
 
 Package.onUse(function(api) { 
     api.versionsFrom('1.0');
 
+    api.use('less');
     api.use('tap:i18n@1.7.0');
 
     api.use('tap:i18n', ['client', 'server']);
     tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
     api.addFiles(tapi18nFiles, ['client', 'server']);
+
+    api.addAssets("client/background/blue1.jpg", "client");
+
+    api.addFiles("client/background.less", "client");
 
     // EXPORT
     
