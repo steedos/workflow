260179fba467957e145eaf993220983adf64aab9
Jack Zhuang <zhuangjianguo@me.com>
2016-10-27 11:36:28 +0800

    build 使用 taobao npm

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 meteor build --server https://cn.steedos.com/workflow --directory /srv/workflow
 cd /srv/workflow/bundle/programs/server
 rm -rf node_modules
-npm install
+rm -y npm-shrinkwrap.json
+npm install --registry https://registry.npm.taobao.org -d
 
 cd /srv/workflow/
 pm2 restart workflow.0
