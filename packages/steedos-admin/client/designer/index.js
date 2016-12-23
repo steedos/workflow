24e199189389d40060b200aa99a0c08b446dcbc5
yinlianghui <yinlianghui@hotoa.com>
2016-12-23 20:23:30 +0800

    #428 为流程设计器传参，把url写活

---
+++ packages/steedos-admin/client/designer/index.js
@@ -0,0 +1,19 @@
+var designer = {
+	urlQuery:function(name){
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return unescape(r[2]);
+		return null; 
+	},
+	run:function(){
+		var space = this.urlQuery("space");
+		var locale = this.urlQuery("locale");
+		if(space && locale){
+			// https://cn.steedos.com/applications/designer/current/zh-cn/?spaceId=53215960334904539e00121a
+			$("#ifrDesigner").attr("src","/applications/designer/current/" + locale + "/?spaceId=" + space);
+		}
+	}
+};
+$(function(){
+	designer.run();
+});
