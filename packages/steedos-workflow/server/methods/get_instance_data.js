d73cf03402d1fe90cc19917c577d1866bdf25d42
sunhaolin <sunhaolin@hotoa.com>
2017-01-03 18:36:18 +0800

    fix 数据串线

--- packages/steedos-workflow/server/methods/get_instance_data.js
+++ packages/steedos-workflow/server/methods/get_instance_data.js
@@ -1,51 +1,53 @@
 Meteor.methods({
 
-  get_instance_data: function (instance_id, formCached, flowCached){
-    console.log("Method get_instance_data " + instance_id +", " + formCached + ", " + flowCached);
+	get_instance_data: function (instance_id, formCached, flowCached) {
+		console.log("Method get_instance_data " + instance_id + ", " + formCached + ", " + flowCached);
 
-    instance = db.instances.findOne(instance_id);
+		check(instance_id, String);
+		check(formCached, Boolean);
+		check(flowCached, Boolean);
 
-    if (!instance) 
-      return {
-        instance: null
-      }
+		var instance = db.instances.findOne(instance_id);
 
-    if (formCached && flowCached)
-      return {
-        instance: instance
-      }
+		if (!instance)
+			return {
+				instance: null
+			};
 
-    if (!formCached)
-    {
-      form = db.forms.findOne(instance.form);
-      form_version = {}
-      if(form.current._id == instance.form_version){
-        form_version = form.current;
-      }
-      else {
-         form_version = _.where(form.historys, {_id: instance.form_version})[0];
-      }
-    }
+		if (formCached && flowCached)
+			return {
+				instance: instance
+			};
+
+		if (!formCached) {
+			var form = db.forms.findOne(instance.form);
+			var form_version = {};
+			if (form.current._id == instance.form_version) {
+				form_version = form.current;
+			}
+			else {
+				form_version = _.where(form.historys, {_id: instance.form_version})[0];
+			}
+		}
 
 
-    if (!flowCached)
-    {
-      flow = db.flows.findOne(instance.flow);
-      flow_version = {}
-      if(flow.current._id == instance.flow_version){
-        flow_version = flow.current;
-      }
-      else {
-         flow_version = _.where(flow.historys, {_id: instance.flow_version})[0];
-      }
-    }
+		if (!flowCached) {
+			var flow = db.flows.findOne(instance.flow);
+			var flow_version = {};
+			if (flow.current._id == instance.flow_version) {
+				flow_version = flow.current;
+			}
+			else {
+				flow_version = _.where(flow.historys, {_id: instance.flow_version})[0];
+			}
+		}
 
-    return {
-      instance: instance,
-      form_version: form_version,
-      flow_version: flow_version
-    }
-    
-  }
+		return {
+			instance: instance,
+			form_version: form_version,
+			flow_version: flow_version
+		}
+
+	}
 
 });
