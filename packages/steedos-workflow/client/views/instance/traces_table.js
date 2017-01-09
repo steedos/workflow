ce6f26561268ce1b4e9bc43fbaa185275adb10eb
baozhoutao <baozhoutao@hotoa.com>
2017-01-09 16:13:43 +0800

    非打印页面，签核历程不显示+

--- packages/steedos-workflow/client/views/instance/traces_table.js
+++ packages/steedos-workflow/client/views/instance/traces_table.js
@@ -1,4 +1,10 @@
-
 Template.instance_traces_table.helpers(TracesTemplate.helpers);
 
 Template.instance_traces_table.events(TracesTemplate.events)
+
+Template.instance_traces_table.onRendered(function () {
+    if (!Session.get("instancePrint")) {
+        $("#tracesCollapse").parent().hide()
+        $("#tracesCollapse").click()
+    }
+})
