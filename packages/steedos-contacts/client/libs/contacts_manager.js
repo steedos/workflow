ac5d0a74880e53b13bdb7fb4e9b57e0a88be561a
Jack Zhuang <zhuangjianguo@me.com>
2017-01-19 13:38:22 +0800

    统一 loading

--- packages/steedos-contacts/client/libs/contacts_manager.js
+++ packages/steedos-contacts/client/libs/contacts_manager.js
@@ -266,7 +266,6 @@ ContactsManager.checkOrgAdmin = function(){
         Session.set('contacts_is_org_admin', true);
         return;
     }
-    $("body").addClass("loading");
     console.log("calling method check_org_admin,orgId:" + orgId);
     Meteor.call('check_org_admin', orgId, function(error, is_suc) {
         if (is_suc) {
