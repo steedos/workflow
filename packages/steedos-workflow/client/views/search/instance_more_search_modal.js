1763caee8dccf336744fc8f3c78ef14838976100
yinlianghui <yinlianghui@hotoa.com>
2017-02-28 18:33:15 +0800

    高级搜索，提交日期的截至日期控件，时间显示不全 #974

--- packages/steedos-workflow/client/views/search/instance_more_search_modal.js
+++ packages/steedos-workflow/client/views/search/instance_more_search_modal.js
@@ -1,13 +1,16 @@
 Template.instance_more_search_modal.onRendered(function() {
-    if(!Steedos.isMobile()){
-        $("#instance_more_search_submit_date_start").datetimepicker({
-            format: "YYYY-MM-DD"
-        });
+	if(!Steedos.isMobile()){
+		$("#instance_more_search_submit_date_start").datetimepicker({
+			format: "YYYY-MM-DD"
+		});
 
-        $("#instance_more_search_submit_date_end").datetimepicker({
-            format: "YYYY-MM-DD"
-        });
-    }
+		$("#instance_more_search_submit_date_end").datetimepicker({
+			format: "YYYY-MM-DD",
+			widgetPositioning:{
+				horizontal: 'right'
+			}
+		});
+	}
 
 	$("#instance_more_search_modal .modal-body").css("max-height", Steedos.getModalMaxHeight());
 	
