0480cd804f7672d42dc779e639c012fa216bf987
yinlianghui <yinlianghui@hotoa.com>
2017-03-03 16:12:07 +0800

    签核历程在手机上增加点击approve行显示详细信息功能 #912，设置modal最大高度

--- packages/steedos-workflow/client/views/instance/trace_detail_modal.js
+++ packages/steedos-workflow/client/views/instance/trace_detail_modal.js
@@ -2,3 +2,7 @@
 Template.instance_trace_detail_modal.helpers(TracesTemplate.helpers);
 
 Template.instance_trace_detail_modal.events(TracesTemplate.events)
+
+Template.instance_trace_detail_modal.onRendered(function () {
+	$(".instance-trace-detail-modal .modal-body").css("max-height", Steedos.getModalMaxHeight());
+})
