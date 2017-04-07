361b6ceb3cd11a17d96a1d2aa283f438007591a1
wangjian <wangjian@hotoa.com>
2017-04-07 18:13:17 +0800

    临时提交

--- packages/steedos-workflow/client/views/instance/flow_steps_modal.js
+++ packages/steedos-workflow/client/views/instance/flow_steps_modal.js
@@ -5,36 +5,54 @@ Template.flow_steps_modal.helpers({
 		var processView = WorkflowManager.getInstanceFlowVersion();
 		var steps = processView.steps;
 		var dealTypeName = "";
-		switch(steps.deal_type) {
-			case '审批时指定人员': 
-				dealTypeName =  "审批时指定人员"
-				break;
-			case '指定人员':
-				dealTypeName =  "指定人员"
-				break;
-			case '指定审批岗位':
-				dealTypeName =  "指定审批岗位"
-				break;
-			case '申请人上级':
-				dealTypeName =  "申请人上级"
-				break;
-			case '申请人':
-				dealTypeName =  "申请人"
-				break;
-			case '指定部门':
-				dealTypeName =  "指定部门"
-				break;
-			default:
-				break;
-		}
 		return steps.map(function(step){
+			
+			var lines = step.lines;
+			var nextStepNames = "";
+			// if(lines){
+			// 	nextStepNames = lines.getProperty("to_step").join(",");
+			// } 
+			// debugger;
+			if(lines){
+				nextStepNames = lines.map(function(line){
+					var to_step = line.to_step;
+					return steps.findPropertyByPK("_id",to_step).name;
+				})
+			}
+			
+			switch(step.deal_type) {
+				case 'pickupAtRuntime': 
+					dealTypeName =  '审批时指定人员';
+					break;
+				case 'specifyUser':
+					dealTypeName =  '指定人员';
+					break;
+				case 'applicantRole':
+					dealTypeName =  '指定审批岗位';
+					break;
+				case 'applicantSuperior':
+					dealTypeName =  '申请人上级';
+					break;
+				case 'applicant':
+					dealTypeName =  '申请人';
+					break;
+				case 'orgField':
+					dealTypeName =  '指定部门';
+					break;
+				default:
+					break;
+			}
 			return {
 				name:step.name,
 				dealTypeName: dealTypeName,
-				lines:step.lines
-			}
+				nextStepNames: nextStepNames
+			};
 		});
 	}
+	// nextStepNames: function(step){
+	// 	return step.lines.getProperty("to_step").join(",")
+
+	// }
 	
 })
 
