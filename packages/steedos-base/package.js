d956d0ce279b2687c5915928039364572e8b0a4d
yinlianghui <yinlianghui@hotoa.com>
2017-04-12 19:19:37 +0800

    实现新的全局moment函数：Steedos.momentFromNow

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -26,6 +26,8 @@ Package.onUse(function(api) {
   api.use('kadira:flow-router@2.10.1');
   api.use('meteorhacks:subs-manager@1.6.4');
 
+  api.use('momentjs:moment@2.14.1');
+
   api.use('tap:i18n@1.8.2');
   api.use('aldeed:simple-schema@1.5.3');
   api.use('aldeed:tabular@1.6.1');
