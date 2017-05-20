f3a09270c5e16e26877edac42211cd2eb3c534b2
yinlianghui <yinlianghui@hotoa.com>
2017-05-20 16:26:34 +0800

    每隔一段时间（如一个月）强行要求重新验证手机号

--- packages/steedos-accounts/package.js
+++ packages/steedos-accounts/package.js
@@ -43,6 +43,7 @@ Package.onUse(function(api) {
 	api.addFiles('lib/core.coffee');
 
 	api.addFiles('server/methods/update_phone.coffee', 'server');
+	api.addFiles('server/methods/disable_phone.coffee', 'server');
 
 	api.addFiles('client/views/accounts_phone.html', 'client');
 	api.addFiles('client/views/accounts_phone.coffee', 'client');
