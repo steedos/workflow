70a1aa356ce5a408f466fabfc51ebf33a98e6f04
yinlianghui <yinlianghui@hotoa.com>
2017-06-14 11:03:55 +0800

    包版本升级steedos:base@0.0.19

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.0.18',
+	version: '0.0.19',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/platform/packages/steedos-theme'
 });
