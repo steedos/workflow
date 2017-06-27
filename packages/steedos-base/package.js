476ff798cc2817f747887d18df38238bf7e319e6
yinlianghui <yinlianghui@hotoa.com>
2017-06-27 18:06:35 +0800

    Steedos.absoluteUrl在前后台两块统一改掉，支持ROOT_URL

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.0.31',
+	version: '0.0.33',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/apps/tree/master/packages/steedos-base'
 });
