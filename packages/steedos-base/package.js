52366adde24c926413272218bb5996cd74ba5caa
yinlianghui <yinlianghui@hotoa.com>
2017-08-11 10:40:18 +0800

    steedos:theme@0.0.20，steedos:accounts@0.0.18，steedos:base@0.0.46

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.0.45',
+	version: '0.0.46',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/apps/tree/master/packages/steedos-base'
 });
