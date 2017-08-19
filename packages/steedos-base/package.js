1972a96a674832677271ce8424a4ddb36d590d83
yinlianghui <yinlianghui@hotoa.com>
2017-08-19 18:52:53 +0800

    steedos:calendar@0.0.16

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.0.52',
+	version: '0.0.53',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/apps/tree/master/packages/steedos-base'
 });
