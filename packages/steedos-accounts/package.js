ceedf088669e34ad067192d6ad5562e5a36284ce
yinlianghui <yinlianghui@hotoa.com>
2017-09-12 11:53:30 +0800

    创建工作区引导画面

--- packages/steedos-accounts/package.js
+++ packages/steedos-accounts/package.js
@@ -59,6 +59,9 @@ Package.onUse(function(api) {
 	api.addFiles('client/views/accounts_phone.html', 'client');
 	api.addFiles('client/views/accounts_phone.coffee', 'client');
 
+	api.addFiles('client/views/accounts_space.html', 'client');
+	api.addFiles('client/views/accounts_space.coffee', 'client');
+
 	api.addFiles('client/views/accounts_phone_verify.html', 'client');
 	api.addFiles('client/views/accounts_phone_verify.coffee', 'client');
 
