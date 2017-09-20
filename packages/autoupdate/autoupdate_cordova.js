89ebe281e1059a8b1e4bf1282d5cafa9c3417a61
sunhaolin <sunhaolin@hotoa.com>
2017-09-20 17:49:58 +0800

    revert

--- packages/autoupdate/autoupdate_cordova.js
+++ packages/autoupdate/autoupdate_cordova.js
@@ -77,10 +77,4 @@ Meteor.startup(function() {
 
 var newVersionAvailable = function() {
   WebAppLocalServer.checkForUpdates();
-};
-
-window.WebAppLocalServer = {};
-WebAppLocalServer.startupDidComplete = function() {};
-WebAppLocalServer.checkForUpdates = function() {};
-WebAppLocalServer.onNewVersionReady = function() {};
-WebAppLocalServer.onError = function() {};
+}
