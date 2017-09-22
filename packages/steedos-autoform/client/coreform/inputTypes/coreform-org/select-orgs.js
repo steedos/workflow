1fae535f9f3fb7a76cf954ca3cce6fbb61da1127
yinlianghui <yinlianghui@hotoa.com>
2017-09-22 18:23:23 +0800

    选组控件click事件改为tap会造成android手机上需要长按才弹出选组控件窗口

--- packages/steedos-autoform/client/coreform/inputTypes/coreform-org/select-orgs.js
+++ packages/steedos-autoform/client/coreform/inputTypes/coreform-org/select-orgs.js
@@ -49,7 +49,7 @@ AutoForm.addInputType("selectorg", {
 
 
 Template.afSelectOrg.events({
-	'tap .selectOrg': function (event, template) {
+	'click .selectOrg': function (event, template) {
 		if (Modal.allowMultiple) {
 			return;
 		}
