b798da1472b787d97baa007b02b1d717c7b4364d
sunhaolin <sunhaolin@hotoa.com>
2017-10-16 10:15:02 +0800

    Revert "Revert "Merge branch 'lml' into develop""
    This reverts commit 90628cb51a487c31957473c3d78b7ef57e60c8ed.

---
+++ packages/steedos-base/lib/models/billing_pay_records.coffee
@@ -0,0 +1,1 @@
+db.billing_pay_records = new Meteor.Collection('billing_pay_records')
--- packages/steedos-workflow/client/lib/workflow_manager.js
+++ packages/steedos-workflow/client/lib/workflow_manager.js
@@ -814,7 +814,7 @@ WorkflowManager.isArrearageSpace = function() {
 	if (space) {
 		if (space.is_paid) {
 
-			return space.balance <= 0.00 ? true : false;
+			return space.end_date <= new Date ? true : false;
 
 		} else {
 			return false;
