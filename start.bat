e2a6910ab9b1b2498110d5bc72b30a0653a50a1d
liminglang <1432915943@qq.com>
2017-11-03 17:31:09 +0800

    calendar版本

--- start.bat
+++ start.bat
@@ -2,5 +2,5 @@ set DB_SERVER=192.168.0.21
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://192.168.0.142:3000/
+set ROOT_URL=http://192.168.0.89:3000/
 meteor run --settings settings.json
