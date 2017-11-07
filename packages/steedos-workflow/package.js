d30b7044324cb6da45eeed5ec195c6b314bf520a
baozhoutao <baozhoutao@hotoa.com>
2017-11-07 16:33:32 +0800

    #1309 新建申请单的流程列表支持星标流程

--- packages/steedos-workflow/package.js
+++ packages/steedos-workflow/package.js
@@ -333,7 +333,8 @@ Package.onUse(function(api) {
 	api.addFiles('server/methods/instance_number_rules.coffee', 'server');
 	api.addFiles('server/methods/check_main_attach.coffee', 'server');
 	api.addFiles('server/methods/related_instances.coffee', 'server');
-	api.addFiles('server/methods/edit_flow_positions.coffee', 'server')
+	api.addFiles('server/methods/edit_flow_positions.coffee', 'server');
+	api.addFiles('server/methods/start_flow.coffee', 'server');
 
 	api.addFiles('server/routes/instance.coffee', 'server');
 	api.addFiles('server/routes/steedos_css.coffee', 'server');
