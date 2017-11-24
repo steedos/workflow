a738fce18973177c94f187f897039c05be3070d2
sunhaolin <sunhaolin@hotoa.com>
2017-11-24 15:22:41 +0800

    fix temp_chunk 找不到时报错

--- packages/cfs-worker/fileWorker.js
+++ packages/cfs-worker/fileWorker.js
@@ -197,7 +197,7 @@ function saveCopy(fsFile, storeName, options) {
     var temp_chunk = FS.TempStore.Tracker.findOne({
       fileId: fsFile._id
     });
-    if (temp_chunk.is_saveCopy) {
+    if (!temp_chunk || temp_chunk.is_saveCopy) {
       return;
     }
     var filepath = path.join(__meteor_bootstrap__.serverDir, '../../../cfs/files/_tempstore/' + temp_chunk.keys["0"]);
