c9e66f149ea2b5d3da3bd03c33c73c0c62cedafb
yinlianghui <yinlianghui@hotoa.com>
2017-12-09 17:24:17 +0800

    #1530 升级版本发包steedos:base@0.0.67

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.0.66',
+	version: '0.0.67',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/apps/tree/master/packages/steedos-base'
 });
