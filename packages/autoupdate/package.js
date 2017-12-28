1592ee7c5e19fc0ec65031adc4ce14717607259a
sunhaolin <sunhaolin@hotoa.com>
2017-12-28 15:17:50 +0800

    add underscore

--- packages/autoupdate/package.js
+++ packages/autoupdate/package.js
@@ -1,29 +1,30 @@
 Package.describe({
-  summary: "Update the client when new client code is available",
-  version: '1.3.12'
+	summary: "Update the client when new client code is available",
+	version: '1.3.12'
 });
 
-Package.onUse(function (api) {
-  api.use([
-    'webapp',
-    'check'
-  ], 'server');
+Package.onUse(function(api) {
+	api.use([
+		'webapp',
+		'check'
+	], 'server');
 
-  api.use([
-    'tracker',
-    'retry'
-  ], 'client');
+	api.use([
+		'tracker',
+		'retry'
+	], 'client');
 
-  api.use([
-    'ddp',
-    'mongo'
-  ], ['client', 'server']);
+	api.use([
+		'ddp',
+		'mongo',
+		'underscore'
+	], ['client', 'server']);
 
-  api.use(['http', 'random'], 'web.cordova');
+	api.use(['http', 'random'], 'web.cordova');
 
-  api.addFiles('autoupdate_server.js', 'server');
-  api.addFiles('autoupdate_client.js', 'web.browser');
-  api.addFiles('autoupdate_cordova.js', 'web.cordova');
+	api.addFiles('autoupdate_server.js', 'server');
+	api.addFiles('autoupdate_client.js', 'web.browser');
+	api.addFiles('autoupdate_cordova.js', 'web.cordova');
 
-  api.export('Autoupdate');
+	api.export('Autoupdate');
 });
