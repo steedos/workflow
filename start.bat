a3f54e57df58fe0c8d2db5c02bee2be688b9583d
sunhaolin <sunhaolin@hotoa.com>
2018-04-04 13:42:55 +0800

    避免切换分支冲突

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
-set DB_SERVER=192.168.0.23
-set MONGO_URL=mongodb://%DB_SERVER%/qhd201711091030
+set DB_SERVER=192.168.0.21
+set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://192.168.0.134:3004/
-meteor run --settings settings.json --port 3004
+set ROOT_URL=http://192.168.0.142:3000/
+meteor run --settings settings.json
