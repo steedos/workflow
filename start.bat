73f117ce0ee9998ef1b333e50dcd0e8e6b75cab0
chenzhipei <chenzhipei@hotoa.com>
2018-05-11 11:21:27 +0800

    packages

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
-set DB_SERVER=192.168.0.21
-set MONGO_URL=mongodb://%DB_SERVER%/steedos
+set DB_SERVER=127.0.0.1
+set MONGO_URL=mongodb://%DB_SERVER%/fssh20180410
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://192.168.0.134:3009/
+set ROOT_URL=http://192.168.0.77:3000/
 set METEOR_DOWN_KEY=down_key
 set METEOR_PACKAGE_DIRS=C:\Users\dell\Documents\GitHub\creator\packages
 set KADIRA_PROFILE_LOCALLY=1
-meteor run --settings settings.json --port 3009
+meteor run --settings settings.json
