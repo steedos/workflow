645690abfe9c4d7ca5d0b6e670f7638c6ba058f5
sunhaolin <sunhaolin@hotoa.com>
2018-09-04 13:27:38 +0800

    大众公用 本地化系统迁移待办事项 #1863

--- packages/steedos-workflow/server/methods/cfs_instances.js
+++ packages/steedos-workflow/server/methods/cfs_instances.js
@@ -1,11 +1,11 @@
 Meteor.methods({
-    cfs_instances_remove: function(file_id) {
+    cfs_instances_remove: function (file_id) {
         check(file_id, String);
         cfs.instances.remove(file_id);
         return true;
     },
 
-    cfs_instances_set_current: function(file_id) {
+    cfs_instances_set_current: function (file_id) {
         check(file_id, String);
         cfs.instances.update({
             _id: file_id
@@ -17,7 +17,7 @@ Meteor.methods({
         return true;
     },
 
-    cfs_instances_lock: function(file_id, user_id, user_name) {
+    cfs_instances_lock: function (file_id, user_id, user_name) {
         cfs.instances.update({
             _id: file_id
         }, {
@@ -30,7 +30,7 @@ Meteor.methods({
         return true;
     },
 
-    cfs_instances_unlock: function(file_id) {
+    cfs_instances_unlock: function (file_id) {
         cfs.instances.update({
             _id: file_id
         }, {
@@ -41,5 +41,28 @@ Meteor.methods({
             }
         });
         return true;
+    },
+
+    download_space_instance_attachments_to_disk: function (spaceId) {
+        check(spaceId, String);
+
+        var store = "instances";
+        var fs = Npm.require('fs');
+        var path = Npm.require('path');
+        var mkdirp = Npm.require('mkdirp');
+        var pathname = path.join(__meteor_bootstrap__.serverDir, '../../../cfs/spaceInstanceAttachments');
+        // Set absolute path
+        var absolutePath = path.resolve(pathname);
+        // Ensure the path exists
+        mkdirp.sync(absolutePath);
+        console.log('absolutePath: ', absolutePath);
+        cfs.instances.find({
+            'metadata.space': spaceId
+        }).forEach(function (c) {
+            var fileName = store + '-' + c._id + '-' + c.name();
+            var filePath = path.join(absolutePath, fileName);
+            console.log('filePath: ', filePath);
+            c.createReadStream(store).pipe(fs.createWriteStream(filePath));
+        })
     }
 })
