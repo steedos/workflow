185ba41e2dc1923ef805f188b8e9f7939eda7384
baozhoutao <baozhoutao@hotoa.com>
2019-01-07 15:44:42 +0800

    autoform-bs-datetimepicke 控件支持date 格式
    https://github.com/steedos/creator/issues/896

--- packages/steedos-autoform-bs-datetimepicker/package.js
+++ packages/steedos-autoform-bs-datetimepicker/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'steedos:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type with timezone support for AutoForm',
-  version: '1.0.6',
+  version: '1.0.7',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datetimepicker.git'
 });
 
