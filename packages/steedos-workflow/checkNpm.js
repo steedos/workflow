bd6c7932a99963886137ab5cb8483207f3044ad6
yinlainghui <yinlianghui@hotoa.com>
2019-01-25 16:33:44 +0800

    #1964 steedos:workflow包替换Npm.require

--- packages/steedos-workflow/checkNpm.js
+++ packages/steedos-workflow/checkNpm.js
@@ -7,14 +7,3 @@ checkNpmVersions({
 	mkdirp: "0.3.x",
 	"sprintf-js": "1.1.2",
 }, 'steedos:workflow');
-
-// checkNpmVersions({
-// 	cookies: "0.6.1",
-// 	ejs: "2.5.5",
-// 	"ejs-lint": "0.2.0",
-// 	"eval": "0.1.2",
-// 	mkdirp: "0.3.5",
-// 	mime: "2.0.2",
-// 	busboy: "0.2.13",
-// 	"node-schedule": "1.2.1"
-// }, 'steedos:workflow');
