01f22587b14816e0fbffa8b7e33c6267d29e3fed
yinlainghui <yinlianghui@hotoa.com>
2019-01-26 14:29:34 +0800

    #1964 steedos:remind包替换Npm.require

--- packages/steedos-remind/package.js
+++ packages/steedos-remind/package.js
@@ -5,16 +5,13 @@ Package.describe({
 	git: ''
 });
 
-Npm.depends({
-	"node-schedule": "1.2.1"
-});
-
 Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
 	api.use('reactive-var');
 	api.use('reactive-dict');
 	api.use('coffeescript');
+	api.use('ecmascript');
 	api.use('random');
 	api.use('ddp');
 	api.use('check');
@@ -26,6 +23,8 @@ Package.onUse(function(api) {
 
 	api.use('steedos:base');
 	api.use('steedos:workflow');
+	
+	api.addFiles('checkNpm.js', 'server');
 
 	api.addFiles('server/ensureIndexes.coffee', 'server');
 	api.addFiles('server/schedule/auto_remind_qhd.coffee', 'server');
