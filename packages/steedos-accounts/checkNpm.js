91dd43e6325229d2c9f24ef59adbea071edcb8d4
yinlainghui <yinlianghui@hotoa.com>
2019-01-28 11:42:55 +0800

    creator #1004 相关包升级发包后引用，'steedos:accounts'包删除，以后在creator中维护

--- packages/steedos-accounts/checkNpm.js
+++
@@ -1,7 +0,0 @@
-import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
-checkNpmVersions({
-	cookies: "0.6.x",
-	phone: "1.0.x",
-	sha256: "0.2.x",
-	"urijs": "1.19.x"
-}, 'steedos:accounts');
