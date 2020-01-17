19b6d4ed84b42e67a2bba90885341eb908d9c018
hotlong <zhuangjianguo@hotoa.com>
2020-01-17 11:08:27 +0800

    fix npm warning

--- packages/steedos-accounts-phone/checkNpm.js
+++ packages/steedos-accounts-phone/checkNpm.js
@@ -3,7 +3,7 @@ require("stream-buffers/package.json");
 
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
-	"phone": "^1.0.12",
-	"twilio": "^1.10.0",
-	"stream-buffers": "^0.2.5"
+	"phone": ">=1.0.12",
+	"twilio": ">=1.10.0",
+	"stream-buffers": ">=0.2.5"
 }, 'steedos:accounts-phone');
